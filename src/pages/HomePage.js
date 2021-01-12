e87a5c22b8808a456ea1890e931196e86a28266f
Su <1668488551@qq.com>
2021-01-12 22:33:42 +0800

    fix: HomePage 返回事件

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator';
 import NavigationUtil from '../utils/NavigationUtil';
+import BackPress from '../components/BackPress';
+import { NavigationActions } from 'react-navigation';
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.backPress = new BackPress({ backPress: () => this.onBackPress() });
+  }
+  componentDidMount() {
+    this.backPress.componentDidMount();
+  }
+  componentWillUnmount() {
+    this.backPress.componentWillUnmount();
+  }
+
+  onBackPress = () => {
+    // console.log(this.props);
+    // const { dispatch, nav } = this.props;
+    // if (nav.routes[1].index === 0) {
+    //   return false;
+    // }
+    // dispatch(NavigationActions.back());
+    return false;
+  }
+
   render() {
     NavigationUtil.navigation = this.props.navigation;
     return <DynamicTabNavigator />;
